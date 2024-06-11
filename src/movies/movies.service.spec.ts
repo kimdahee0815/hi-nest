ffb41e76f24838c70f3cfdd886657c146d4cc8bd
kimdahee0815 <billionairedahee@gmail.com>
2024-06-11 15:37:49 +0900

    test: unit test for deleteOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -44,4 +44,25 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
