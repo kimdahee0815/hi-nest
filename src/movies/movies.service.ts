7a951db3f4fc76f2ee6fac304aa5b6c5e241c18c
kimdahee0815 <billionairedahee@gmail.com>
2024-06-11 13:17:28 +0900

    refactor: getOne with error handling

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './movies/movie.entity';
 
 @Injectable()
@@ -10,12 +10,14 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) throw new NotFoundException(`Movie with ID ${id} NOT FOUND!`);
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
+  deleteOne(id: string) {
+    this.getOne(id);
     this.movies = this.movies.filter((movie) => movie.id !== +id);
-    return true;
   }
 
   create(movieData) {
